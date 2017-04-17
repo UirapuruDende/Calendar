9a327af946ae587f0bcc50fda77baf44a1bc8352
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2017-04-17 18:06:28 +0100

    command changes

--- Application/Command/CreateCalendarCommand.php
+++ Application/Command/CreateCalendarCommand.php
@@ -1,10 +1,23 @@
 <?php
 namespace Dende\Calendar\Application\Command;
 
+use Dende\Calendar\Domain\IdInterface;
+
 final class CreateCalendarCommand
 {
+    /**
+     * @var IdInterface
+     */
+    public $calendarId;
+
     /**
      * @var string
      */
     public $title = '';
+
+    public function __construct(IdInterface $calendarId, string $title)
+    {
+        $this->calendarId = $calendarId;
+        $this->title = $title;
+    }
 }
