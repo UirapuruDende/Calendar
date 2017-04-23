b5cca447d0a9c9316939530bf719b64953b420b5
Grzegorz 'Uirapuru' Kaszuba <uirapuruadg@gmail.com>
2017-04-23 18:59:13 +0100

    up

--- Application/Command/UpdateOccurrenceCommand.php
+++ Application/Command/UpdateOccurrenceCommand.php
@@ -2,26 +2,62 @@
 namespace Dende\Calendar\Application\Command;
 
 use DateTime;
-use Dende\Calendar\Domain\Calendar\Event\Occurrence\OccurrenceDuration;
 use Dende\Calendar\Domain\Calendar\Event\Occurrence\OccurrenceId;
 
 /**
  * Class UpdateOccurrenceCommand.
  */
-final class UpdateOccurrenceCommand
+class UpdateOccurrenceCommand
 {
     /**
-     * @var OccurrenceId
+     * @var string
      */
-    public $occurrenceId;
+    protected $occurrenceId;
 
     /**
      * @var DateTime
      */
-    public $startDate;
+    protected $startDate;
 
     /**
-     * @var OccurrenceDuration
+     * @var DateTime
      */
-    public $duration;
+    protected $endDate;
+
+    /**
+     * UpdateOccurrenceCommand constructor.
+     * @param string $occurrenceId
+     * @param DateTime $startDate
+     * @param DateTime $endDate
+     */
+    public function __construct(string $occurrenceId, DateTime $startDate, DateTime $endDate)
+    {
+        $this->occurrenceId = $occurrenceId;
+        $this->startDate = $startDate;
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @return OccurrenceId
+     */
+    public function occurrenceId(): string
+    {
+        return $this->occurrenceId;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function startDate(): DateTime
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function endDate(): DateTime
+    {
+        return $this->endDate;
+    }
 }
